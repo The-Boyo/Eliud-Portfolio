41d6440093482cb3c226cefa5ac4fbf09fad73d3
Eliud Metto <eliudmetto93@gmail.com>
2022-12-04 19:47:08 +0300

    Updated Intro.js
    Changed the spelling of the word fruition

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -20,7 +20,7 @@ const Intro = () => {
         <h2 className="name-title">Eliud here,</h2>
         <h4 className="title-about">About Me</h4>
         <p className="para-about">
-          I'm a Front-End Developer well adept in <strong>HTML, CSS, JavaScript, React </strong> as well as version control system <strong> Git</strong> and also <strong>GitHub</strong>. Not forgetting, I provide that glossy UI/UX touch that gives users a hearty experience when interacting with your site. Your ideas and thoughts are mine to execute and bringing into fruitition what you picture is my endeavour. 
+          I'm a Front-End Developer well adept in <strong>HTML, CSS, JavaScript, React </strong> as well as version control system <strong> Git</strong> and also <strong>GitHub</strong>. Not forgetting, I provide that glossy UI/UX touch that gives users a hearty experience when interacting with your site. Your ideas and thoughts are mine to execute and bringing into fruition what you picture is my endeavour. 
         </p>
       </div>
     </section>
