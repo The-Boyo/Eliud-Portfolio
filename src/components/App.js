c5a384b8e7818f4135be403f29d416d6081b4e5e
Eliud Metto <eliudmett093@gmail.com>
2024-12-25 18:37:30 +0300

    Changed the app backround

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Skills from "./skills/Skills";
 import Work from "./work/Work";
 import Projects from "./projects/Projects";
 import { ThemeStore } from "./store/ThemeStore";
+import RingsBackground from "./RingsBackground";
 
 const App = () => {
 	const [allHeight, setAll] = useState(null);
@@ -31,27 +32,7 @@ const App = () => {
 				<Projects />
 
 				{/* Animated background divs */}
-
-				<div className="back one"></div>
-				<div className="back two"></div>
-				<div className="back three"></div>
-				<div className="back four"></div>
-				<div className="back five"></div>
-				<div className="back six"></div>
-				<div className="back seven"></div>
-				<div className="back eight"></div>
-				<div className="back nine"></div>
-				<div className="back ten"></div>
-				<div className="back eleven"></div>
-				<div className="back twelve"></div>
-				<div className="back thirteen"></div>
-				<div className="back fourteen"></div>
-				<div className="back fifteen"></div>
-				<div className="back sixteen"></div>
-				<div className="back seventeen"></div>
-				<div className="back eighteen"></div>
-				<div className="back nineteen"></div>
-				<div className="back twenty"></div>
+				<RingsBackground />
 
 				<footer className="end">
 					<p className="closing">
